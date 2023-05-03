f8fc55a7eb69e942cbd977cfa5df0124064e681c
markfaust123 <markfaust123@gmail.com>
2023-05-03 16:22:34 -0400

    bounds class created with setter/getters functions and constructor. Fields also set

--- bounds.h
+++ bounds.h
@@ -4,9 +4,25 @@
 class Bounds {
 private:
   // TODO: add fields
+  double xmax;
+  double xmin;
+  double ymax;
+  double ymin;
 
 public:
   // TODO: add member functions
+  Bounds(double xmax, double xmin, double ymax, double ymin) : xmax(xmax), xmin(xmin), ymax(ymax), ymin(ymin) { }
+
+  double get_x_max() { return xmax; }
+  double get_x_min() { return xmin; }
+  double get_y_max() { return ymax; }
+  double get_y_min() { return ymin; }
+
+  double set_x_max(double xmax) { this->xmax = xmax; }
+  double set_x_min(double xmin) { this->xmin = xmin; }
+  double set_y_max(double ymax) { this->ymax = ymax; }
+  double set_y_min(double ymin) { this->ymin = ymin; }
+
 };
 
 #endif // BOUNDS_H
